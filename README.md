588831de6c9df6b762b78514ffab7bfbb8207071
jauma <hola@juanmanavarro.es>
2015-05-25 13:22:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Angular boilerplate
+
+Simple angular boilerplate.
+
+- [Folder structure](https://scotch.io/tutorials/angularjs-best-practices-directory-structure)
---
+++ app/components/home/homeController.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.controllers', [])
+        .controller('HomeController', function() {
+            var vm = this;
+
+            vm.title = 'About us';
+        });
+
+})();
