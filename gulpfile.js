1c0e66d29b64bcaca7ee7155a673a03f85099ab5
Juanma Navarro <hola@juanmanavarro.es>
2015-06-26 19:48:33 +0200

    refactor folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
+    minifyCss = require('gulp-minify-css'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify');
 
 // gulp
 gulp.task('default', ['watch']);
@@ -10,12 +15,32 @@ gulp.task('reload', function() {
 });
 
 gulp.task('jshint', function() {
-    return gulp.src('app/**/*.js')
+    return gulp.src('src/app/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
-})
+});
 
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch(['app/**/*', 'index.html'], ['reload', 'jshint']);
+    gulp.watch(['src/app/**/*', 'src/index.html'], ['reload', 'jshint']);
 });
+
+gulp.task('compress', function() {
+    gulp.src('src/index.html')
+        .pipe(useref.assets())
+        .pipe(gulpif('*.js', uglify({mangle: false})))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', function() {
+    gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+    gulp.src('libs/font-awesome/fonts/**')
+        .pipe(gulp.dest('dist/fonts'));
+    gulp.src('src/images/**')
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('build', ['compress', 'copy']);
