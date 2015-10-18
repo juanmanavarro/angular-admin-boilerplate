0de65ee37ad8b1667141b161e8445f44d67a1708
Juanma Navarro <hola@juanmanavarro.es>
2015-10-18 10:55:06 +0200

    fix some bugs

---
+++ src/app/components/home/home.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.controllers', [])
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = [];
+    function HomeController() {
+        var vm = this;
+
+        vm.title = 'Working';
+    }
+
+})();
